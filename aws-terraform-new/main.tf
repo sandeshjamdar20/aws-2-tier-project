7f706f5f8c861c6600c8c688c5047fb072dc78d6
sandeshjamdar20 <sandeshjamdar51@gmail.com>
2026-06-02 13:13:40 +0530

    Update main.tf

--- aws-terraform-new/main.tf
+++ aws-terraform-new/main.tf
@@ -14,7 +14,7 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket  = "terraformbuckerforstatefiles" # create s3 bucket to store statefile
+    bucket  = "sandesh2-terraform-statefile" # create s3 bucket to store statefile
     key     = "dev/terraform.tfstate"
     region  = "us-east-1"
     encrypt = false

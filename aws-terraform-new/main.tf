34aba3d558c5bc29d28d8034c59b14d34fda93bc
sandeshjamdar20 <sandeshjamdar51@gmail.com>
2026-06-03 08:58:52 +0530

    Update main.tf

--- aws-terraform-new/main.tf
+++ aws-terraform-new/main.tf
@@ -73,7 +73,7 @@ module "rds" {
   allocated_storage       = 20
   storage_type            = "gp2"
   engine                  = "MySQL"
-  engine_version          = "8.0.41"
+  engine_version          = "8.0"
   instance_class          = "db.t3.micro"
   backup_retention_period = 0
 }
